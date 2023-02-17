ffcb3e1089ec5e7d4969c9719bab8de9c978d8bb
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-02-17 18:58:56 -0300

    finished project

--- app/src/main/java/br/edu/ifsp/ads/cadastro/Formulario.java
+++ app/src/main/java/br/edu/ifsp/ads/cadastro/Formulario.java
@@ -1,2 +1,86 @@
-package br.edu.ifsp.ads.cadastro;public class Formulario {
+package br.edu.ifsp.ads.cadastro;
+
+import android.widget.RadioButton;
+import android.widget.Spinner;
+
+public class Formulario {
+
+    private String full_name;
+
+    private String phone;
+
+    private String email;
+
+    private String sexo;
+
+    private String city;
+
+    private String uf;
+
+    public Formulario(String full_name, String phone, String email, String sexo, String city, String uf) {
+        this.full_name = full_name;
+        this.phone = phone;
+        this.email = email;
+        this.sexo = sexo;
+        this.city = city;
+        this.uf = uf;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRadio() {
+        return sexo;
+    }
+
+    public void String(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome='" + full_name + '\'' +
+                ", telefone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", sexo=" + sexo +
+                ", cidade='" + city + '\'' +
+                ", uf=" + uf;
+    }
 }
--- app/src/main/java/br/edu/ifsp/ads/cadastro/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/ads/cadastro/MainActivity.java
@@ -3,12 +3,78 @@ package br.edu.ifsp.ads.cadastro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameTx;
+
+    private EditText phoneTx;
+
+    private EditText emailTx;
+
+    private CheckBox emailCk;
+
+    private RadioGroup radioGroupSx;
+
+    private EditText cityTx;
+
+    private Spinner spinnerUf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameTx = findViewById(R.id.inputNome);
+        phoneTx = findViewById(R.id.inputTelefone);
+        emailTx = findViewById(R.id.inputEmail);
+        cityTx = findViewById(R.id.inputCidade);
+        emailCk = findViewById(R.id.checkBox);
+        spinnerUf = findViewById(R.id.ufSp);
+
+        RadioGroup group = (RadioGroup) findViewById(R.id.sexo);
+
+        Button save = findViewById(R.id.button2); // botão de salvar
+
+        Button clear = findViewById(R.id.button); // botão de limpar
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                emailCk.setChecked(false);
+                nameTx.setText("");
+                phoneTx.setText("");
+                cityTx.setText("");
+                cityTx.setText("");
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Formulario form;
+                RadioButton button = (RadioButton) group.findViewById(R.id.radioButtonM);
+                if (button.isActivated()) {
+                    form = new Formulario(nameTx.getText().toString(), phoneTx.getText().toString(), emailTx.getText().toString(),
+                            button.getText().toString(), cityTx.getText().toString(), spinnerUf.getSelectedItem().toString());
+                } else {
+                    button = (RadioButton) group.findViewById(R.id.radioButtonF);
+                    form = new Formulario(nameTx.getText().toString(), phoneTx.getText().toString(), emailTx.getText().toString(),
+                            button.getText().toString(), cityTx.getText().toString(), spinnerUf.getSelectedItem().toString());
+                }
+                Toast.makeText(getApplicationContext(), form.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
